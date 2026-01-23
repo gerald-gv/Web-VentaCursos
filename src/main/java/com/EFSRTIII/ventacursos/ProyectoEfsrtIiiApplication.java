e32739686decaf3dbc76f655e324e45d5221216a
gerald-gv <geraldgutierrez918@gmail.com>
2026-01-23 15:32:10 -0500

    Primer Commit - Spring

---
+++ src/main/java/com/EFSRTIII/ventacursos/ProyectoEfsrtIiiApplication.java
@@ -0,0 +1,13 @@
+package com.EFSRTIII.ventacursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoEfsrtIiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoEfsrtIiiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/EFSRTIII/ventacursos/ProyectoEfsrtIiiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EFSRTIII.ventacursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoEfsrtIiiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
