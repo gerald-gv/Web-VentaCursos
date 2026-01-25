098abf2b1a2fe3c19d53de66e7de04c23a9a07da
gerald-gv <geraldgutierrez918@gmail.com>
2026-01-25 01:25:23 -0500

    Apartado Inicio Culminado

---
+++ src/main/java/com/EFSRTIII/ventacursos/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package com.EFSRTIII.ventacursos.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "pages/home";
+    }
+
+}
